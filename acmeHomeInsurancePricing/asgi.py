58b8ac511062e46f54ca5ff227c47f489c788ecc
tobywiedenhoefer <toby.wiedenh@gmail.com>
2023-10-13 19:55:47 -0500

    Renamed project

--- acmeHomeInsurancePricing/asgi.py
+++ acmeHomeInsurancePricing/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for acmeHomeinsurancePricing project.
+ASGI config for acmeHomeInsurancePricing project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acmeHomeinsurancePricing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acmeHomeInsurancePricing.settings')
 
 application = get_asgi_application()
