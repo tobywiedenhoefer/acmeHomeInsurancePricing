8f5cb9e306250ba046219ca83cbedd266c32f964
tobywiedenhoefer <toby.wiedenh@gmail.com>
2023-10-15 21:55:35 -0500

    Setting debug to True for demo purposes

--- acmeHomeInsurancePricing/settings.py
+++ acmeHomeInsurancePricing/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7z96u&e-=-g2&jo%d)#t2)+zd%_e5o8u&q5lt2%ygst3t1=le7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
